49ac45b7ff7ea2067ef11dce8a328205132ee5f0
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-26 14:03:19 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class sessionVerifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = sessionVerifyWorker()
+    print("Worker engine initialized.")
